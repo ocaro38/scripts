5860c9111b4aa01b03c7d060bd8bfefebbfcaa78
ocaro38 <34098738+ocaro38@users.noreply.github.com>
2021-03-11 12:54:22 -0500

    Update createVMs.ps1

--- createVMs.ps1
+++ createVMs.ps1
@@ -1,3 +1,4 @@
+#Creates new VM, Defines Resource Group, Name, VirtualNetwork, Subnet, Image, Size
 New-AzVm `
  -ResourceGroupName "vm-networks" `
  -Name "dataProcStage1" `
@@ -5,3 +6,10 @@ New-AzVm `
  -SubnetName "vm-servers" `
  -image "Win2016Datacenter" `
  -Size "Standard_DS2_v2"
+ 
+#Creates a variable called nic.
+#Sets nic to the Vm Interface based on hostname of VM and Resource Group
+#Select IpConfigurations.publicipaddress.id properties of $nic and set them to null
+$nic = Get-AzNetworkInterface -Name dataProcStage2 -ResourceGroup vm-networks
+$nic.IpConfigurations.publicipaddress.id = $null
+Set-AzNetworkInterface -NetworkInterface $nic
